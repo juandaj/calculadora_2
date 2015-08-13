a89f3a74ad659fc517f04c74108292d29bafeaa1
test <a@b.com>
2015-08-13 12:22:19 -0500

    se añadieron las operaciones a calculadora.c

---
+++ calculadora.c
@@ -0,0 +1,28 @@
+#include "calculadora.h"
+
+int suma(int a, int b){
+
+return a+b;
+
+}
+
+
+int resta(int a, int b){
+
+return a-b;
+
+}
+
+
+int division(int a, int b){
+
+return a/b;
+
+}
+
+
+int multiplicacion(int a, int b){
+
+return a*b;
+
+}
